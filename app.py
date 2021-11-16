3107e993bffe11e171a2bbf50d88ad9e59627bf8
adityamodi11 <92273126+adityamodi11@users.noreply.github.com>
2021-11-16 23:46:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,177 @@
+from numpy import true_divide
+import streamlit as st
+from streamlit.proto.Progress_pb2 import Progress
+import helper
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+import pandas as pd
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import plotly.io as pio
+
+monthly_price = pd.read_csv('monthly_price.csv')
+state = monthly_price['state'].to_list()
+commodity = monthly_price['commodity'].to_list()
+
+st.sidebar.title("Inflation Analyzer")
+st.sidebar.image('inflation.jpg')
+st.sidebar.text("-By Aditya Modi")
+
+user_menu = st.sidebar.radio(
+    'Select Page',
+    ('Home','Analyze','Data')
+)
+
+if user_menu == 'Home':
+    st.title("Home")
+    st.header("Here You will learn Key terms to analyze inflation!")
+    st.text("- By Aditya Modi")
+    st.image("in.jpg",use_column_width = True)
+    st.text("To Skip this, select another Page....")
+
+    st.subheader("Inflation")
+    st.write("""Inflation is an economic indicator that indicates the rate of rising prices of goods and services in the economy. 
+              Ultimately it shows the decrease in the buying power of the rupee. It is measured as a percentage.This percentage indicates the increase or decrease from the previous period. Inflation can be a cause of concern as
+            the value of money keeps decreasing as inflation rises.""")
+    st.video("https://youtu.be/beAvFHP4wDI")
+    st.subheader("Cause of Inflation")
+    st.write("""
+    Monetary Policy: It determines the supply of currency in the market. Excess supply 
+                                 of money leads to inflation. Hence decreasing the value of the currency.
+
+    Fiscal Policy: It monitors the borrowing and spending of the economy. Higher borrowings (debt),
+                           result in increased taxes and additional currency printing to repay the debt.
+
+    Demand-pull Inflation: Increases in prices due to the gap between the demand (higher) and supply (lower).
+
+    Cost-push Inflation: Higher prices of goods and services due to increased cost of production.
+
+    Exchange Rates: Exposure to foreign markets are based on the dollar value. 
+                            Fluctuations in the exchange rate have an impact on the rate of inflation. """)
+
+    st.subheader("Consumer Price Index")
+    st.write("""The Consumer Price Index (CPI) is a measure that examines the weighted average of prices of a basket of consumer goods and services, such as transportation, food, and medical care. It is calculated by taking price changes for each item in the predetermined basket of goods and averaging them. Changes in the CPI are used to assess price changes associated with the cost of living.
+                The CPI is one of the most frequently used statistics for identifying periods of inflation or deflation. It may be compared with the producer price index (PPI), which instead of considering prices paid by consumers looks at what businesses pay for inputs.""")
+
+    st.subheader("Wholesale Price Index")
+    st.write("""A wholesale price index (WPI) is an index that measures and tracks the changes in the price of goods in the stages before the retail level. This refers to goods that are sold in bulk and traded between entities or businesses (instead of between consumers). Usually expressed as a ratio or percentage, the WPI shows the included goods'
+               average price change; it is often seen as one indicator of a country's level of inflation.""")
+
+    st.subheader("Correlation")
+    st.write("""Correlation shows the strength of a relationship between two variables and is expressed numerically by the correlation coefficient. The correlation coefficient's values range between -1.0 and 1.0.
+                A perfect positive correlation means that the correlation coefficient is exactly 1. This implies that as one security moves, either up or down, the other security moves in lockstep, in the same direction. A perfect negative correlation means that two assets move in opposite directions, while a zero correlation implies no linear relationship at all.""")
+state = ['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Goa',
+       'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu Kashmir',
+       'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh',
+       'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland',
+       'National Capital', 'Orissa', 'Punjab', 'Rajasthan', 'Sikkim',
+       'Tamil Nadu', 'Telangana', 'Tripura', 'Union Territories',
+       'Uttar Pradesh', 'Uttarakhand', 'West Bengal']
+
+
+commodity = ['Moong', 'Gram', 'Mustard Oil', 'Onion', 'Potato', 'Ragi',
+       'Red Chillies', 'Rice', 'Salt', 'Sugar', 'Suji', 'Tea', 'Apple',
+       'Arhar', 'Atta', 'Bajra', 'Banana', 'Besan', 'Biscuit',
+       'Groundnut Oil', 'Gur', 'Jowar', 'Maida', 'Maize', 'Masur', 'Meat',
+       'Milk', 'Coriander', 'Cummin Seed', 'Eggs', 'Fish', 'Ghee',
+       'Gingelly Oil', 'Black Pepper', 'Bread', 'Brinjal', 'Butter',
+       'Chicken', 'Coconut', 'Coconut Oil', 'Coffee', 'Tomato',
+       'Turmeric', 'Urad', 'Vanaspati', 'Wheat', 'Gents Dhoti', 'Saree',
+       'Glycodin', 'Ink', 'Kerosene Oil', 'Kettle', 'Long Cloth',
+       'Match Box', 'Pant Cloth', 'Paper', 'Pencil', 'Pent Cloth',
+       'Razor Blade', 'Sandals-Ladies', 'Chappal-Rubber',
+       'Saridon/Anacin', 'Septran', 'Shirting', 'Shoes-Gents',
+       'Soft Cake', 'Tawa', 'Tooth Paste', 'Tooth Powder', 'Torch Cell',
+       'Tumbler', 'Vics Vaporub', 'Washing Soap', 'Washing soda',
+       'Bathing Soap', 'Burnol', 'Cement', 'Chappal-Ladies', 'Crocin',
+       'Cycle Tube', 'Cycle Tyre', 'Detergent Powder', 'Diesel',
+       'Electric bulb', 'Exercise Book']
+
+
+if user_menu == 'Analyze':
+
+    st.header("Select the Credentials")
+    col1,col2 = st.columns(2)
+    
+    a =col1.selectbox("State",state)
+    b = col2.selectbox("Product",commodity)
+    col3 = st.slider("Year",min_value=2001,max_value=2021,step=1)
+    button = st.button("Submit")
+    if button:
+        st.success("Submitted!")  
+        progress = st.progress(0)
+        for i in range(100):
+            time.sleep(0.01)
+            progress.progress(i+1)
+        
+        st.balloons()
+        
+        st.subheader("Commodity Price Over Years")
+        st.plotly_chart(helper.price_comm(a,b))
+
+        st.subheader("Inflation Over Years")
+        st.plotly_chart(helper.inflation(a,b))
+
+        # st.subheader("Inflation Over Years")
+        st.plotly_chart(helper.inf_df(a,b))
+
+        st.subheader("Diesel & Petrol vs Commodity")
+        st.plotly_chart(helper.fuel_economy2(a,b)[0])
+        st.write("The Correlation b/w diesel and Commodity Price is ",helper.fuel_economy2(a,b)[1])
+        st.plotly_chart(helper.fuel_economy(a,b)[0])
+        st.write("The Correlation b/w Petrol and Commodity Price is ",helper.fuel_economy(a,b)[1])
+
+
+        st.subheader("Price at different States")
+        st.text("Expand the map from the top-right corner for better view.")
+        st.text("Price(Rs 0) means data is not avaiable for tha region")
+        st.plotly_chart(helper.state_wise_price(b,col3))
+        st.plotly_chart(helper.state_wise(b,col3))
+
+
+        st.subheader("State vs State comparison")
+        st.plotly_chart(helper.state_comp(b,col3,a))
+        st.plotly_chart(helper.state_comp_df(b,col3,a))
+
+        st.subheader("Oil Economy")
+        st.plotly_chart(helper.oil_economy())
+
+        st.subheader("Oil Inflation")
+        st.plotly_chart(helper.oil_inf())
+
+pet = pd.read_csv("petrol.csv")
+die = pd.read_csv("diesel.csv")
+
+if user_menu == 'Data':
+
+    st.title("Data Sets(Sample df)")
+    
+    st.subheader("Food and Non-Food Retail Price")
+    st.markdown(""" You can download the complete data from this [link](https://www.kaggle.com/kk9969/retail-prices-of-commodities-in-india?select=Monthly_Non_Food_Retail_Prices.csv)""",True)
+    st.dataframe(helper.monthly_price_df.head(1000))
+
+    st.subheader("Diesel Price")
+    st.markdown("""You can download the complete data both petrol and diesel from this [link](https://www.kaggle.com/sudhirnl7/fuel-price-in-india)""",True)
+    st.dataframe(die.head(1000))
+
+    st.subheader("Petrol Price")
+    st.dataframe(pet.head(1000))
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+# st.plotly_chart(helper.oil_inf())
---
+++ helper.py
@@ -0,0 +1,485 @@
+from numpy.core.fromnumeric import size
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import plotly.io as pio
+import zipfile
+
+zf = zipfile. ZipFile('ezyzip.zip')
+
+
+monthly_price_df = pd. read_csv(zf. open('monthly_price.csv'))
+
+# state = monthly_price_df['state'].unique().to_list()
+# commodity = monthly_price_df['commodity'].unique().to_list()
+"""
+---------------------------------------------------------------------------------------------------------------------------------------
+# Commodity Price Over Years
+---------------------------------------------------------------------------------------------------------------------------------------
+"""
+
+
+def price_comm(state,commodity):
+    cent = monthly_price_df[monthly_price_df['state'] == state].groupby(['year','commodity']).mean()
+    cent = pd.DataFrame(cent)
+    cent = cent.reset_index()
+    
+    comm = cent[cent['commodity'] == commodity]
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x= comm['year'],
+        y= comm['retail_price(in Rs.)'],
+        opacity = 0.6,
+        # hovertemplate="%{y}%{_xother}",
+        marker_color = 'purple'
+    ))
+
+    fig.add_trace(go.Scatter(
+        x= comm['year'],
+        y= comm['retail_price(in Rs.)'],
+        line = dict(shape = 'linear', color = 'rgb(205, 12, 24)', width= 3, dash = 'dot'),
+        mode = "lines+markers",
+        marker = dict(symbol = "circle", color = 'rgb(205, 12, 24)',size = 6),
+        connectgaps = True,
+        # hovertemplate="%{y}%{_xother}"
+    ))
+
+    fig.update_layout(plot_bgcolor = "white",hovermode="x unified",
+        title={
+        'text': "Price over Years",
+        'y':0.9,
+        'x':0.44,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+        xaxis_title="Year",
+        yaxis_title="Price (in Rs)",
+        legend_title="Legend Title",
+        font=dict(
+        family="Courier New, monospace",
+        size=20,
+        color="RebeccaPurple"
+    ))
+
+    return fig
+    # fig.show()
+
+# price_comm('Jharkhand','Mustard Oil')
+
+
+
+
+"""
+---------------------------------------------------------------------------------------------------------------------------------------
+# Inflation over years
+---------------------------------------------------------------------------------------------------------------------------------------
+"""
+
+def inflation(state,commodity):
+    cent = monthly_price_df[monthly_price_df['state'] == state].groupby(['year','commodity']).mean()
+    cent = pd.DataFrame(cent)
+    cent = cent.reset_index()
+    
+    comm = cent[cent['commodity'] == commodity].copy()
+    price_arr = comm['retail_price(in Rs.)'].shift(periods =1)
+    comm['inflation(in %)'] = ((comm['retail_price(in Rs.)'] - price_arr)/comm['retail_price(in Rs.)']) *100
+    
+    fig1 = px.bar(comm, x='year', y='inflation(in %)')
+    fig1.update_layout(plot_bgcolor = "white",hovermode="x unified",
+        title={
+        'text': "Inflation(%) over Years",
+        'y':0.9,
+        'x':0.48,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+        xaxis_title="Year",
+        yaxis_title="Inflation(in %)",
+        legend_title="Legend Title",
+        font=dict(
+        family="Courier New, monospace",
+        size=20,
+        color="RebeccaPurple"
+    ))
+    # fig.show()
+    
+    return fig1
+    # fig.show()
+
+def inf_df(state,commodity):
+    cent = monthly_price_df[monthly_price_df['state'] == state].groupby(['year','commodity']).mean()
+    cent = pd.DataFrame(cent)
+    cent = cent.reset_index()
+    
+    comm = cent[cent['commodity'] == commodity].copy()
+    price_arr = comm['retail_price(in Rs.)'].shift(periods =1)
+    comm['inflation(in %)'] = ((comm['retail_price(in Rs.)'] - price_arr)/comm['retail_price(in Rs.)']) *100
+
+    df = comm.reset_index()
+    df = df.loc[:,['year','retail_price(in Rs.)','inflation(in %)']]
+    df = df.round(2)
+    fig = ff.create_table(df,index=False)
+    for i in range(len(fig.layout.annotations)):
+        fig.layout.annotations[i].font.size = 20
+
+    return fig
+
+
+
+
+# inflation('Jharkhand','Mustard Oil')
+
+
+"""
+---------------------------------------------------------------------------------------------------------------------------------------
+# Diesel & Petrol vs Commodity
+---------------------------------------------------------------------------------------------------------------------------------------
+"""
+
+
+diesel_df = pd.read_csv('diesel.csv')
+petrol_df = pd.read_csv('petrol.csv')
+diesel_df['date'] = pd.to_datetime(diesel_df['date'])
+diesel_df['year'] = diesel_df['date'].dt.year
+
+petrol_df['date'] = pd.to_datetime(petrol_df['date'])
+petrol_df['year'] = petrol_df['date'].dt.year
+
+pet_df = petrol_df.iloc[:,2:]
+die_df = diesel_df.iloc[:,2:]
+pet_df = pet_df.groupby('year').mean()
+die_df = die_df.groupby('year').mean()
+pet_df.columns = ['petrol(in Rs.)']
+die_df.columns = ['diesel(in Rs.)']
+
+
+
+def fuel_economy(state,commodity):
+    cent = monthly_price_df.groupby(['year','commodity']).mean()
+    cent = pd.DataFrame(cent)
+    cent = cent.reset_index()
+    
+    comm = cent[cent['commodity'] == commodity].copy()
+    
+#     comm = comm.iloc[1:-1]
+    comm.set_index('year',inplace = True)
+    combine = pd.concat([comm,pet_df,die_df],axis = 1)
+    combine.reset_index(inplace = True)
+
+    crr = combine['retail_price(in Rs.)'].corr(combine['petrol(in Rs.)'])
+    
+    
+    fig = px.scatter(combine, y="retail_price(in Rs.)", x="petrol(in Rs.)",color = 'year',
+                     hover_data=['year'])
+    fig.update_traces(marker_size=10)
+    return [fig,crr]
+
+def fuel_economy2(state,commodity):
+    cent = monthly_price_df.groupby(['year','commodity']).mean()
+    cent = pd.DataFrame(cent)
+    cent = cent.reset_index()
+    
+    comm = cent[cent['commodity'] == commodity].copy()
+    
+#     comm = comm.iloc[1:-1]
+    comm.set_index('year',inplace = True)
+    combine = pd.concat([comm,pet_df,die_df],axis = 1)
+    combine.reset_index(inplace = True)
+
+    crr = combine['retail_price(in Rs.)'].corr(combine['diesel(in Rs.)'])
+
+    fig = px.scatter(combine, y="retail_price(in Rs.)", x="diesel(in Rs.)",color = 'year',
+                     hover_data=['year'])
+    fig.update_traces(marker_size=10)
+    return [fig,crr]
+
+
+
+    
+# print("The Correlation b/w Petrol and Commodity Price is ",combine['retail_price(in Rs.)'].corr(combine['petrol(in Rs.)']))
+# print("The Correlation b/w Diesel and Commodity Price is ",combine['retail_price(in Rs.)'].corr(combine['diesel(in Rs.)']))
+
+# fuel_economy('Maharashtra','Mustard Oil')
+
+
+
+
+
+"""
+---------------------------------------------------------------------------------------------------------------------------------------
+# Price at different States
+---------------------------------------------------------------------------------------------------------------------------------------
+"""  
+
+def state_wise_price(commodity,year):
+    cent = monthly_price_df[monthly_price_df['commodity'] == commodity]
+    cent = cent.groupby(['state','year']).mean()
+    cent = pd.DataFrame(cent)
+    cent.reset_index(inplace = True)
+    cent = cent.iloc[:,:3]
+    cent = cent[cent['year'] == year]
+    cent['state'].replace({'Jammu Kashmir' : 'Jammu & Kashmir',
+                            'Orissa' : 'Odisha',
+                             'National Capital' : 'Delhi'},inplace = True)
+    
+    a = {'state':'Chhattisgarh', 'year':year,'retail_price(in Rs.)':0}
+    b = {'state':'Ladakh', 'year':year,'retail_price(in Rs.)':0}
+    cent = cent.append(a, ignore_index = True)
+    cent = cent.append(b,ignore_index = True)
+    cent['retail_price(in Rs.)'] = cent['retail_price(in Rs.)'].fillna(0)
+    
+    
+    fig = px.choropleth(
+    cent,
+    geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
+    featureidkey='properties.ST_NM',
+    locations='state',
+    color='retail_price(in Rs.)',
+    labels={'retail_price(in Rs.)':'retail_price(in Rs.)'},
+     color_continuous_scale='Viridis',
+   )
+
+    fig.update_geos(fitbounds="locations", visible=False)
+
+    # fig.show()
+    
+    
+
+    return fig
+
+def state_wise(commodity,year):
+
+    cent = monthly_price_df[monthly_price_df['commodity'] == commodity]
+    cent = cent.groupby(['state','year']).mean()
+    cent = pd.DataFrame(cent)
+    cent.reset_index(inplace = True)
+    cent = cent.iloc[:,:3]
+    cent = cent[cent['year'] == year]
+    cent['state'].replace({'Jammu Kashmir' : 'Jammu & Kashmir',
+                            'Orissa' : 'Odisha',
+                             'National Capital' : 'Delhi'},inplace = True)
+    
+    a = {'state':'Chhattisgarh', 'year':year,'retail_price(in Rs.)':0}
+    b = {'state':'Ladakh', 'year':year,'retail_price(in Rs.)':0}
+    cent = cent.append(a, ignore_index = True)
+    cent = cent.append(b,ignore_index = True)
+    cent['retail_price(in Rs.)'] = cent['retail_price(in Rs.)'].fillna(0)
+
+
+
+    df = cent
+    df = df.round(2)
+    fig2 = ff.create_table(df,index=False)
+    for i in range(len(fig2.layout.annotations)):
+        fig2.layout.annotations[i].font.size = 20
+
+    return fig2
+
+# state_wise_price('Moong',2021)
+
+
+
+"""
+---------------------------------------------------------------------------------------------------------------------------------------
+# State vs State comparison¶
+---------------------------------------------------------------------------------------------------------------------------------------
+"""  
+
+
+def state_comp(commodity,year,state):
+    cent = monthly_price_df[monthly_price_df['commodity'] == commodity]
+    cent = cent.groupby(['state','year']).mean()
+    cent = pd.DataFrame(cent)
+    cent.reset_index(inplace = True)
+    cent = cent.iloc[:,:3]
+    cent = cent[cent['retail_price(in Rs.)'].notnull()]
+    cent = cent[cent['year'] == year]
+    a = cent['retail_price(in Rs.)'].idxmax()
+    b = cent['retail_price(in Rs.)'].idxmin()
+    c = cent.index[cent['state'] == state].tolist()[0]
+    my_list = [a,b,c]
+#     reqd_df = cent.iloc[[a,b,c]]
+    Filter_df  = cent[cent.index.isin(my_list)]
+    
+    fig = go.Figure(go.Bar(
+            y= Filter_df['state'],
+            x= Filter_df['retail_price(in Rs.)'],
+            orientation='h',
+            marker=dict(
+        color='rgba(164, 163, 204, 0.85)',
+        line=dict(
+            color='rgba(50, 171, 96, 1.0)',
+            width=0.5),
+            )))
+    fig.update_layout(plot_bgcolor = "white",hovermode="x unified",
+        title={
+        'text': "Given State vs Min and Max State",
+        'y':0.9,
+        'x':0.48,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+        xaxis_title="Retail Price(in Rs.)",
+        yaxis_title="Sate",
+        font=dict(
+        family="Courier New, monospace",
+        size=20,
+        color="RebeccaPurple"
+    ))
+
+    # fig.show()
+    
+    
+    fig2 = ff.create_table(Filter_df,index=False)
+    for i in range(len(fig2.layout.annotations)):
+        fig2.layout.annotations[i].font.size = 20
+
+    # fig.show()
+
+    return fig
+
+def state_comp_df(commodity,year,state):
+    cent = monthly_price_df[monthly_price_df['commodity'] == commodity]
+    cent = cent.groupby(['state','year']).mean()
+    cent = pd.DataFrame(cent)
+    cent.reset_index(inplace = True)
+    cent = cent.iloc[:,:3]
+    cent = cent[cent['retail_price(in Rs.)'].notnull()]
+    cent = cent[cent['year'] == year]
+    a = cent['retail_price(in Rs.)'].idxmax()
+    b = cent['retail_price(in Rs.)'].idxmin()
+    c = cent.index[cent['state'] == state].tolist()[0]
+    my_list = [a,b,c]
+
+    Filter_df  = cent[cent.index.isin(my_list)]
+    fig2 = ff.create_table(Filter_df,index=False)
+    for i in range(len(fig2.layout.annotations)):
+        fig2.layout.annotations[i].font.size = 20
+    
+    return fig2
+
+
+
+
+    
+
+# state_comp('Mustard Oil',2021,'Jharkhand')
+
+
+"""
+---------------------------------------------------------------------------------------------------------------------------------------
+# OIL Economy
+---------------------------------------------------------------------------------------------------------------------------------------
+"""
+diesel_df['my'] = diesel_df['date'].dt.to_period('M')
+die = diesel_df.groupby('my').mean()
+
+petrol_df['my'] = petrol_df['date'].dt.to_period('M')
+pet = petrol_df.groupby('my').mean()
+
+merge_df = pd.merge(left = die, right = pet,left_index = True, right_index = True)
+merge_df = merge_df.iloc[:,:-1]
+merge_df.columns = ['diesel','year','petrol']
+
+    
+
+
+def oil_economy():
+    
+    fig = go.Figure()
+    merge_df.index = merge_df.index.astype('str')   
+    fig.add_trace(go.Scatter(
+            x= merge_df.index,
+            y= merge_df['diesel'],
+            line = dict(shape = 'linear', color = 'rgb(205, 12, 24)', width= 3, dash = 'dot'),
+            mode = "lines+markers",
+            marker = dict(symbol = "circle", color = 'rgb(205, 12, 24)',size = 2),
+            connectgaps = True,
+            hovertemplate="%{y}%{_xother}",
+            name = 'diesel'
+        ))
+
+    fig.add_trace(go.Scatter(
+            x= merge_df.index,
+            y= merge_df['petrol'],
+            line = dict(shape = 'linear', color = 'blue', width= 3, dash = 'dot'),
+            mode = "lines+markers",
+            marker = dict(symbol = "circle", color = 'blue',size = 2),
+            connectgaps = True,
+            hovertemplate="%{y}%{_xother}",
+            name = 'petrol'
+        ))
+
+    fig.update_layout(plot_bgcolor = "white",hovermode="x unified",
+            title={
+            'text': "Price over Years",
+            'y':0.9,
+            'x':0.44,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+            xaxis_title="Year",
+            yaxis_title="Price (in Rs)",
+            legend_title="OIL",
+            font=dict(
+            family="Courier New, monospace",
+            size=20,
+            color="RebeccaPurple"
+        
+        ))
+        
+        
+    return fig
+
+# oil_economy()
+
+
+def oil_inf():
+    diesel_df['my'] = diesel_df['date'].dt.to_period('M')
+    die = diesel_df.groupby('my').mean()
+
+    petrol_df['my'] = petrol_df['date'].dt.to_period('M')
+    pet = petrol_df.groupby('my').mean()
+
+    merge_df = pd.merge(left = die, right = pet,left_index = True, right_index = True)
+    merge_df = merge_df.iloc[:,:-1]
+    merge_df.columns = ['diesel','year','petrol']
+
+    merge_df.index = merge_df.index.astype('str')  
+    merge_df = merge_df.groupby('year').mean()
+    price_arr = merge_df['diesel'].shift(periods =1)
+    merge_df['diesel_inflation(in %)'] = ((merge_df['diesel'] - price_arr)/merge_df['diesel'] ) *100
+    price_arr = merge_df['petrol'].shift(periods =1)
+    merge_df['petrol_inflation(in %)'] = ((merge_df['petrol'] - price_arr)/merge_df['petrol'] ) *100
+    merge_df = merge_df.iloc[1:]
+
+    #fig = px.bar(merge_df, x=merge_df.index, y='diesel_inflation(in %)')
+    fig = go.Figure(data=[
+        go.Bar(name='diesel', x=merge_df.index, y= merge_df['diesel_inflation(in %)']),
+        go.Bar(name='petrol', x=merge_df.index, y= merge_df['petrol_inflation(in %)'])
+    ])
+    fig.update_layout(plot_bgcolor = "white",hovermode="x unified",
+            title={
+            'text': "Inflation(%) over Years",
+            'y':0.9,
+            'x':0.48,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+            xaxis_title="Year",
+            yaxis_title="Inflation(in %)",
+            legend_title="Legend Title",
+            font=dict(
+            family="Courier New, monospace",
+            size=20,
+            color="RebeccaPurple"
+        ))
+
+    return fig
+
+    # fig.show()
+
+# oil_inf()
+
+
